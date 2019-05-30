d5d6e8465bc20ef87f4ae47ac6db859099c0990b
Alex N'Guessan <alexmarcaureln@gmail.com>
2019-05-30 18:49:53 +0000

    clean up the spec file

--- spec/matrice_spec.rb
+++ spec/matrice_spec.rb
@@ -1,6 +1,7 @@
 # TODO 
 # [ ] puissance
 # [ ] transposée
+# [ ] carree?
 
 require './lib/matrice.rb'
 
@@ -10,27 +11,27 @@ describe 'matrice' do
     matriceC = [[1, 4, -2], [2, 7, 3]]
     matriceD = [[5, -1, 6], [-2, 1, -3]]
 
-    it 'return the right matrix A*B' do
-        matrice = [[-1, 2, 1], [-1, -5, -3]]
-        expect(produit(matriceA, matriceB)).to eq matrice
+    context 'Basic operations' do 
+        it 'Multiplication' do
+            expect(produit(matriceA, matriceB)).to eq [[-1, 2, 1], [-1, -5, -3]]
+        end
+
+        it 'Addition' do 
+            expect(addition(matriceC,matriceD)).to eq [[6, 3, 4], [0, 8, 0]]
+        end
+
+        it 'Scalar multiplication' do 
+            expect(produit_scalaire(matriceD, -1)).to eq [[-5, 1, -6], [2, -1, 3]]
+        end
     end
 
-    it 'should return false because B*A does not exist' do
-        expect(produit(matriceB, matriceA)).to eq false
-    end
+    context 'Verify possible operations' do
+        it 'Multiplication?' do
+            expect(produit?(matriceB, matriceA)).to eq false
+        end
 
-    it 'return the right matrix C+D' do 
-        matrice = [[6, 3, 4], [0, 8, 0]]
-        expect(addition(matriceC,matriceD)).to eq matrice
-    end
-
-    it 'should return false because matrix B and D do not have the same format' do 
-        expect(addition(matriceB, matriceD)).to eq false
-    end
-
-    it 'return the right matrix -D' do 
-        matriceD = [[5, -1, 6], [-2, 1, -3]]
-        matrice = [[-5, 1, -6], [2, -1, 3]]
-        expect(produit_scalaire(matriceD, -1)).to eq matrice
+        it 'Addition?' do 
+            expect(addition?(matriceB, matriceD)).to eq false
+        end
     end
 end
